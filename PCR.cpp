3e06057d126b89497533e0a83732cfae679f1417
Max <roszondas@yandex.ru>
2017-04-17 17:23:50 +0300

    Add Order-struct and output method for CWorld
    Медленно выстраивается будущая структура кода. Пока реализован только вывод наружу.

--- PCR.cpp
+++ PCR.cpp
@@ -1,3 +1,5 @@
+#pragma GCC optimize "O3,omit-frame-pointer,inline"
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,7 +8,14 @@
 
 using namespace std;
 
-#define ARGS_NUM        4
+#define ARGS_NUM            4
+#define ENTITY_TYPES_NUM    4
+
+
+enum {CMD_MOVE, CMD_FIRE, CMD_MINE, CMD_LEFT, CMD_RIGHT, CMD_FASTER, CMD_SLOWER, CMD_WAIT};
+enum {SHIP, BARREL, EXPLOSION, MINE};
+string entity_name[ENTITY_TYPES_NUM] {"SHIP", "BARREL", "CANNONBALL", "MINE"};
+int entity_type[ENTITY_TYPES_NUM] {SHIP, BARREL, EXPLOSION, MINE};
 
 
 ///////////////////////////////////// Point
@@ -32,7 +41,13 @@ struct Point {
 
 
 ///////////////////////////////////// Entity
-struct Entity
+struct EntityBase
+{
+    bool explosion = false;
+    int args[2];
+};
+
+struct Entity : public EntityBase
 {
     int id;
     string type;
@@ -41,11 +56,82 @@ struct Entity
 };
 
 
+///////////////////////////////////// Order
+struct Order
+{
+public:
+    Order() {};
+    
+    Order(int cmd, int x, int y) {
+        this->cmd = cmd;
+        this->x = x;
+        this->y = y;
+        }
+    
+    Order(int cmd) {
+        this->cmd = cmd;
+    }
+    
+    void Message (string msg) {
+        this->msg = msg;
+    }
+    
+    string Out() {
+        string ret = "";
+        
+        switch (cmd) {
+            case CMD_MOVE:
+                ret +=  (string) "MOVE "+
+                to_string(x) +   " "    + 
+                to_string(y);
+                break;
+            case CMD_FIRE:
+                ret +=  (string) "FIRE "+
+                to_string(x) +   " "    + 
+                to_string(y);
+                break;
+            case CMD_MINE:
+                ret +=  (string) "MINE";
+                break;
+            case CMD_LEFT:
+                ret +=  (string) "PORT";
+                break;
+            case CMD_RIGHT:
+                ret +=  (string) "STARBOARD";
+                break;
+            case CMD_FASTER:
+                ret +=  (string) "FASTER";
+                break;
+            case CMD_SLOWER:
+                ret +=  (string) "SLOWER";
+                break;
+            
+            case CMD_WAIT:
+            default:
+                ret +=  (string) "WAIT";
+        }
+
+        if(!msg.empty())
+            ret += " " + msg;
+
+        return ret;
+    };
+
+
+private:
+    int cmd = -1;
+    int x = -1, y = -1;
+    string msg = "";
+};
+
+
 
 ///////////////////////////////////// CWorld
 class CWorld{
 private:
-
+    void Output(vector <Order> &orders);
+    void DecisionMainSystem(vector <Order> &orders);
+    
 public:
     CWorld() {};
     void UpdateEntity(Entity entity);
@@ -53,38 +139,74 @@ public:
 };
 
 
-
 void CWorld::UpdateEntity(Entity entity)
 {
-
+    int type = -1;
+    
+    for(int i = 0; i < ENTITY_TYPES_NUM; i++) {
+        if(entity.type == entity_name[i]){
+            type = i;
+            break;
+        }
+    }
+    
+    switch(type){
+        case SHIP :   break;
+        default   :   break;
+    }
+    
 }
 
 
 void CWorld::MakeTurn(int myShipCount)
 {
+    vector <Order> orders;
+        
     for (int i = 0; i < myShipCount; i++) {
-            cout << "WAIT" << endl;
+        DecisionMainSystem(orders);
+        orders.back().Message(to_string(i));
+    }
+    
+    this->Output(orders);
+}
+
+
+void CWorld::DecisionMainSystem(vector <Order> &orders)
+{
+    int shipOrder, shipTargetX, shipTargetY;
+    
+    shipOrder = CMD_MOVE;
+    shipTargetX = rand() % 23;
+    shipTargetY = rand() % 21;
+    
+    orders.push_back(Order(shipOrder, shipTargetX, shipTargetY));
+}
+
+
+void CWorld::Output(vector <Order> &orders)
+{
+    for (auto it = orders.begin(); it < orders.end(); it++)
+    {
+        cout << it->Out();
+        cout << "\n";
     }
 }
 
 
 ///////////////////////////////////// Main
 int main() {
-    int skip = 0;
     int myShipCount;
     int entityCount;
+    Entity entity;
+    CWorld World;
     
     srand (time(NULL));
-    
-    CWorld World;
     
     while (1) {
         cin >> myShipCount; cin.ignore();
         cin >> entityCount; cin.ignore();
         
         for (int i = 0; i < entityCount; i++) {
-            Entity entity;
-            
             cin >> entity.id >> entity.type >> entity.coords.x >> entity.coords.y;
             
             for(int i = 0; i < ARGS_NUM; i++)
